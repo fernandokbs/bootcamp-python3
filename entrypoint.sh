fd58482d1f78675652a105949862840db29975c9
Fernando <fernando93d@gmail.com>
2025-05-18 15:27:24 -0600

    Nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,6 @@ python /shopping_cart/shopping_cart/manage.py seed_data --carts 3
 
 echo "Iniciando Gunicorn..."
 
+cd shopping_cart
+
 exec gunicorn shopping_cart.wsgi:application --bind 0.0.0.0:8000
